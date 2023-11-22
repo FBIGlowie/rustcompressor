577fa34fb28bc00195d7c2cf86df8ebbd83872ab
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2023-11-21 21:53:40 -0500

    Used rustfmt to clean it up and it looks so readable

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,71 @@
-use std::{env,fs::{self,File},process::exit,io::{prelude::*},path::Path};
 use colored::Colorize;
+use std::{
+    env,
+    fs::{self, File},
+    io::prelude::*,
+    path::Path,
+    process::exit,
+};
 
-use flate2::Compression;
 use flate2::write::GzEncoder;
-
+use flate2::Compression;
 
 fn main() {
-    println!("Rust Compressor, only uses {} compression type, only works in current dir", "deflate".cyan());
+    println!(
+        "Rust Compressor, only uses {} compression type, only works in current dir",
+        "deflate".cyan()
+    );
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        
-        1 => {println!("Please input parameters like this, {} {} {}", "rustcompressor".yellow(), "input.xyz".blue(), "output.xyz".red());},
-        2 => {println!("Please input parameters like this, {} {} {}", "rustcompressor".yellow(), "input.xyz".blue(), "output.xyz".red());},
-        3 => {println!("Compressing {} into {}{}", &args[1].blue(), &args[2].red(), ".gz".red()); check_files(args);},
-        _ => todo!()
+        1 => {
+            println!(
+                "Please input parameters like this, {} {} {}",
+                "rustcompressor".yellow(),
+                "input.xyz".blue(),
+                "output.xyz".red()
+            );
+        }
+        2 => {
+            println!(
+                "Please input parameters like this, {} {} {}",
+                "rustcompressor".yellow(),
+                "input.xyz".blue(),
+                "output.xyz".red()
+            );
+        }
+        3 => {
+            println!(
+                "Compressing {} into {}{}",
+                &args[1].blue(),
+                &args[2].red(),
+                ".gz".red()
+            );
+            check_files(args);
+        }
+        _ => todo!(),
     }
 }
 
 fn check_files(args: Vec<String>) {
-    let input_file: String  = args[1].to_string();
+    let input_file: String = args[1].to_string();
     let output_file: String = args[2].to_string();
     let output_file_gz: String = format!("{}.gz", &output_file);
-    
-    if Path::new(&output_file).exists() || Path::new(&output_file_gz).exists()  {
-        panic!("{}", "Output file already exists, please delete or choose a different name".red());
+
+    if Path::new(&output_file).exists() || Path::new(&output_file_gz).exists() {
+        panic!(
+            "{}",
+            "Output file already exists, please delete or choose a different name".red()
+        );
     } else if Path::new(&input_file).exists() {
-            println!("<====25%====");  readandcompress(&input_file, &output_file);
-        } else {
-            panic!("{}", "Input file doesn't exist in current dir".red());
-        }
+        println!("<====25%====");
+        readandcompress(&input_file, &output_file);
+    } else {
+        panic!("{}", "Input file doesn't exist in current dir".red());
     }
+}
 
-
-fn readandcompress(input_file: &String, output_file: &String) { 
-    let output_file:String = format!("{}.gz", output_file);
+fn readandcompress(input_file: &String, output_file: &String) {
+    let output_file: String = format!("{}.gz", output_file);
     if let Ok(mut input_file) = File::open(input_file) {
         // Read the contents of the file into a Vec<u8>
         print!("50%");
@@ -45,12 +77,13 @@ fn readandcompress(input_file: &String, output_file: &String) {
             let compressed_data = e.finish().unwrap();
             print!("====75%");
             if let Ok(mut output_file) = File::create(output_file) {
-                
                 if let Err(err) = output_file.write_all(&compressed_data) {
                     eprintln!("Error writing to output file: {}", err);
-                } else { 
-                    print!("====100%>"); println!("DONE!"); exit(1);}
-           
+                } else {
+                    print!("====100%>");
+                    println!("DONE!");
+                    exit(1);
+                }
             } else {
                 eprintln!("Error creating output file");
             }
@@ -60,5 +93,6 @@ fn readandcompress(input_file: &String, output_file: &String) {
     } else {
         eprintln!("Error opening input file");
     }
-    }
-    
+}
+
+/*  */
