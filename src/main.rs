43faba53becd24040662fc347bfaa96ae2b45eb0
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2023-11-21 22:17:10 -0500

    ahhh this is so hard

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use colored::Colorize;
-use std::{
-    env,
-    fs::{self, File},
-    io::prelude::*,
-    path::Path,
-    process::exit,
-};
+use std::{env, fs::File, path::Path, process::exit, io::{Read,Write}};
+
 
 use flate2::write::GzEncoder;
 use flate2::Compression;
