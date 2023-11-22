b7bb8fec415d82ffcdc4ed72d18b1043684abdfb
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2023-11-21 20:25:44 -0500

    Final version, may not come back, switched libs and fixed some stupid Option String error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{env,fs::{self,File},process::exit,io::{Read,Write}};
+use std::{env,fs::{self,File},process::exit,io::{prelude::*},path::Path};
 use colored::Colorize;
-use deflate::deflate_bytes;
+
+use flate2::Compression;
+use flate2::write::GzEncoder;
+
+
 fn main() {
     println!("Rust Compressor, only uses {} compression type, only works in current dir", "deflate".cyan());
     let args: Vec<String> = env::args().collect();
@@ -8,54 +12,44 @@ fn main() {
         
         1 => {println!("Please input parameters like this, {} {} {}", "rustcompressor".yellow(), "input.xyz".blue(), "output.xyz".red());},
         2 => {println!("Please input parameters like this, {} {} {}", "rustcompressor".yellow(), "input.xyz".blue(), "output.xyz".red());},
-        3 => {println!("Compressing {} into {}", &args[1].blue(), &args[2].red()); check_files(args);},
+        3 => {println!("Compressing {} into {}{}", &args[1].blue(), &args[2].red(), ".gz".red()); check_files(args);},
         _ => todo!()
-    };
+    }
 }
 
 fn check_files(args: Vec<String>) {
     let input_file: String  = args[1].to_string();
     let output_file: String = args[2].to_string();
-
-    if let Ok(current_dir) = env::current_dir() {
-        match fs::read_dir(current_dir) {
-            Ok(dir) => {
-                 // first check if output file exists and stop execution
-
-                for entry in &mut dir.filter_map(Result::ok) {
-
-                    if entry.file_name().to_str() == Some(&output_file) {
-                        panic!("{}", "Output file already exists, please delete or choose a different name".red());
-                    } 
-
-                    if entry.file_name().to_str() == Some(&input_file) {
-                        println!("<25%===="); readandcompress(&input_file, &output_file)
-
-                    } 
-                } }
-            Err(err) => eprintln!("Error reading directory: {}", err),
+    let output_file_gz: String = format!("{}.gz", &output_file);
+    
+    if Path::new(&output_file).exists() || Path::new(&output_file_gz).exists()  {
+        panic!("{}", "Output file already exists, please delete or choose a different name".red());
+    } else if Path::new(&input_file).exists() {
+            println!("<====25%====");  readandcompress(&input_file, &output_file);
+        } else {
+            panic!("{}", "Input file doesn't exist in current dir".red());
         }
-    } else {
-        eprintln!("Error getting current working directory");
     }
-}
+
 
 fn readandcompress(input_file: &String, output_file: &String) { 
-    let output_file:String = output_file.to_string();
+    let output_file:String = format!("{}.gz", output_file);
     if let Ok(mut input_file) = File::open(input_file) {
         // Read the contents of the file into a Vec<u8>
         print!("50%");
         let mut file_data = Vec::new();
         if let Ok(_) = input_file.read_to_end(&mut file_data) {
             // Now you have the file data in the `file_data` vector
-            let compressed_data = deflate_bytes(&file_data);
-            print!("====75%")
-            if let Ok(mut output_file) = File::create(output_file + ".deflate") {
+            let mut e = GzEncoder::new(Vec::new(), Compression::default());
+            e.write_all(file_data.as_mut_slice()).unwrap();
+            let compressed_data = e.finish().unwrap();
+            print!("====75%");
+            if let Ok(mut output_file) = File::create(output_file) {
                 
                 if let Err(err) = output_file.write_all(&compressed_data) {
                     eprintln!("Error writing to output file: {}", err);
-                } else {
-                    print!("====100%>"); exit(1);}
+                } else { 
+                    print!("====100%>"); println!("DONE!"); exit(1);}
            
             } else {
                 eprintln!("Error creating output file");
